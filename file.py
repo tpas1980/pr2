263c1148c1ff5db28ab73b402fa6040265683f23
tpas1980 <afsane.alipour1393@gmail.com>
2024-11-16 22:34:40 +0330

    Create file.py

---
+++ file.py
@@ -0,0 +1,13 @@
+A = int(input("your score: "))
+
+if A in [20, 19, 18, 17]:
+    print("your score A")
+
+elif A in [16, 15, 14]:
+    print("your score B")
+
+elif A in [13, 12, 11, 10]:
+    print("your score C")
+
+elif A in [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]:
+    print("your score F")
